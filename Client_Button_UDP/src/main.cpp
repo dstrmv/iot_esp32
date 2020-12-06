5c92401fabc99514cd537eda73be73ab85534ea5
Ivan Bulychev <dstrmv@outlook.com>
2020-12-06 13:58:20 +0400

    UDP Client

---
+++ Client_Button_UDP/src/main.cpp
@@ -0,0 +1,48 @@
+#include "WiFi.h"
+#include "AsyncUDP.h"
+
+const char * ssid = "Keenetic-0219";
+const char * password = "EMGbmepp";
+
+const int BTN = 18;
+bool connected = false;
+bool light = false;
+
+AsyncUDP udp;
+
+void setup()
+{
+    pinMode(BTN, INPUT);
+    Serial.begin(115200);
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("WiFi Failed");
+        while(1) {
+            delay(1000);
+        }
+    }
+
+    connected = udp.connect(IPAddress(192,168,1,53), 1234);
+    delay(500);
+
+}
+
+void loop()
+{
+    delay(1);
+    if (digitalRead(BTN) == HIGH && !light) {
+        Serial.println("Btn on");
+        light = true;
+        udp.print("111");
+    }
+
+    if (digitalRead(BTN) == LOW && light) {
+        Serial.println("Btn off");
+        light = false;
+        udp.print("000");
+    }
+
+    delay(1);
+}
