b17afe07dbba3b704d85c6c38df83e03751929d9
Ivan Bulychev <dstrmv@outlook.com>
2020-12-06 14:00:55 +0400

    UDP Client fix

--- Client_Button_UDP/src/main.cpp
+++ Client_Button_UDP/src/main.cpp
@@ -1,8 +1,8 @@
 #include "WiFi.h"
 #include "AsyncUDP.h"
 
-const char * ssid = "Keenetic-0219";
-const char * password = "EMGbmepp";
+const char *ssid = "Keenetic-0219";
+const char *password = "EMGbmepp";
 
 const int BTN = 18;
 bool connected = false;
@@ -17,31 +17,40 @@ void setup()
     WiFi.disconnect();
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
         Serial.println("WiFi Failed");
-        while(1) {
+        while (1)
+        {
             delay(1000);
         }
     }
 
-    connected = udp.connect(IPAddress(192,168,1,53), 1234);
+    connected = udp.connect(IPAddress(192, 168, 1, 53), 1234);
     delay(500);
-
 }
 
 void loop()
 {
     delay(1);
-    if (digitalRead(BTN) == HIGH && !light) {
+    if (digitalRead(BTN) == HIGH && !light)
+    {
         Serial.println("Btn on");
         light = true;
-        udp.print("111");
+        if (connected)
+        {
+            udp.print("111");
+        }
     }
 
-    if (digitalRead(BTN) == LOW && light) {
+    if (digitalRead(BTN) == LOW && light)
+    {
         Serial.println("Btn off");
         light = false;
-        udp.print("000");
+        if (connected)
+        {
+            udp.print("000");
+        }
     }
 
     delay(1);
